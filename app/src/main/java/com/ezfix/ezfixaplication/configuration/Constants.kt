ee911ef3ece3c5850d27ce338d6f6cdaaf83aa7c
italo-de-souza <italo.souza@bandtec.com.br>
2022-04-27 12:57:04 -0300

    mudança da URL

--- app/src/main/java/com/ezfix/ezfixaplication/configuration/Constants.kt
+++ app/src/main/java/com/ezfix/ezfixaplication/configuration/Constants.kt
@@ -1,6 +1,6 @@
 package com.ezfix.ezfixaplication.configuration
 
 class Constants {
-    val IP_BACKEND = "http://192.168.15.11:8080"
-//    val IP_BACKEND = "https://api.ezfix.com.br/"
+//    val IP_BACKEND = "http://192.168.15.11:8080"
+    val IP_BACKEND = "https://api.ezfix.com.br/"
 }
